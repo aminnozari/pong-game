e31b85bea7fee10840cdf8adf83ce63b7dfd4672
Amin Nozari <41434431+aminnozari@users.noreply.github.com>
2022-03-02 15:44:19 +0330

    Add files via upload

---
+++ scripts/AI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    float speed = 30;
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float ball_y = ball.transform.position.y;
+        float ball_x = ball.transform.position.x; 
+        float rocket_y = transform.position.y;
+        float rocket_h = GetComponent<BoxCollider2D>().transform.localScale.y;
+        float direction;
+
+        if(ball_x < 0)
+        {
+           // if() // if movement is negative:
+           // {
+                if(ball_y + rocket_h < rocket_y) //if the ball is lower, go down.
+                {
+                    direction = -1;
+                }
+                else if(ball_y - rocket_h > rocket_y) //if the ball is higher, go up.
+                {
+                    direction = 1;
+                }
+                else //if it's ok, do nothing.
+                {
+                    direction = 0;
+                }
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(0, direction) * speed;
+           // }
+        }
+    }
+}
